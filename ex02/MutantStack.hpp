04d329ab675a28b2c3f49f232f14332855b94b08
vfurmane <vfurmane@student.42.fr>
2022-01-18 13:39:23 +0100

    feat: add ex02

---
+++ ex02/MutantStack.hpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   MutantStack.hpp                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 09:48:17 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/01/18 13:37:43 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iterator>
+
+template<typename T, typename Container = std::deque<T> >
+class MutantStack : public std::stack<T, Container>
+{
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+		typename MutantStack<T>::container_type::iterator		begin()
+		{
+			return this->c.begin();
+		}
+		typename MutantStack<T>::container_type::const_iterator	begin() const
+		{
+			return this->c.begin();
+		}
+		typename MutantStack<T>::container_type::iterator		end()
+		{
+			return this->c.end();
+		}
+		typename MutantStack<T>::container_type::const_iterator	end() const
+		{
+			return this->c.end();
+		}
+};
+
+#endif
---
+++ ex02/main.cpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/18 09:45:52 by vfurmane          #+#    #+#             */
+/*   Updated: 2022/01/18 10:42:12 by vfurmane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include "MutantStack.hpp"
+
+int	main(void)
+{
+	MutantStack<int> mstack;
+	mstack.push(5);
+	mstack.push(17);
+	std::cout << mstack.top() << std::endl;
+	mstack.pop();
+	std::cout << mstack.size() << std::endl;
+	mstack.push(3);
+	mstack.push(5);
+	mstack.push(737);
+	//[...]
+	mstack.push(0);
+	MutantStack<int>::iterator it = mstack.begin();
+	MutantStack<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite)
+	{
+		std::cout << *it << std::endl;
+		++it;
+	}
+	std::stack<int> s(mstack);
+	return 0;
+}
