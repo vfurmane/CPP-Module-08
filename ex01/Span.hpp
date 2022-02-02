f8b3999ed76c0ff503a170a3c7162ba74c4076fa
vfurmane <vfurmane@student.42.fr>
2022-02-02 10:23:27 +0100

    feat [ex01]: add more tests and add a Span::fill function

--- ex01/Span.hpp
+++ ex01/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/17 11:49:33 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/01/18 09:44:26 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/02/02 10:16:32 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,6 +30,11 @@ class Span
 		void	addNumber(int nbr);
 		int		shortestSpan(void) const;
 		int		longestSpan(void) const;
+		void	fill(std::list<int>::const_iterator begin, std::list<int>::const_iterator end)
+		{
+			for (std::list<int>::const_iterator it = begin; it != end; ++it)
+				this->addNumber(*it);
+		}
 
 		class Overflow : public std::exception
 		{
--- ex01/main.cpp
+++ ex01/main.cpp
@@ -6,22 +6,62 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/18 08:58:07 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/01/18 09:32:57 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/02/02 10:22:29 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
+#include <cstdlib>
+#include <time.h>
 #include "Span.hpp"
+
+#define MAX_SPAN_SIZE 10000
+
+std::list<int>	*generateRandomList(unsigned int N)
+{
+	std::list<int>	*list = new std::list<int>;
+
+	srand(time(NULL));
+	for (unsigned int i = 0; i < N; i++)
+		list->push_back(rand() % N);
+	return list;
+}
 
 int	main(void)
 {
-	Span sp = Span(5);
-	sp.addNumber(5);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
+	{
+		std::list<int>	*list = generateRandomList(10);
+		std::list<int>::const_iterator	begin = list->begin();
+		std::list<int>::const_iterator	end = list->end();
+		std::list<int>::const_iterator	it;
+		for (it = begin; it != end; ++it)
+			std::cout << *it << std::endl;
+		std::cout << "==========" << std::endl;
+		Span	sp(10);
+		sp.fill(list->begin(), list->end());
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		delete list;
+	}
+	std::cout << "==========" << std::endl;
+	{
+		std::list<int>	*list = generateRandomList(MAX_SPAN_SIZE);
+		Span	sp(MAX_SPAN_SIZE);
+		sp.fill(list->begin(), list->end());
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+		delete list;
+	}
+	std::cout << "==========" << std::endl;
+	{
+		Span sp = Span(5);
+		sp.addNumber(5);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+	}
 	return 0;
 }
