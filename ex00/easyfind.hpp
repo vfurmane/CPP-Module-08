2ac293802dc798feff3c83c6cc292bd4177a3fcf
vfurmane <vfurmane@student.42.fr>
2022-02-05 11:27:21 +0100

    fix [ex00]: use std::find instead of iterators

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -6,27 +6,19 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:06:06 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/01/16 14:38:24 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/02/04 14:27:55 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef EASYFIND_HPP
 # define EASYFIND_HPP
 
-# include <cstddef>
+# include <algorithm>
 
 template<typename T>
 typename T::const_iterator	easyfind(const T &container, int to_find)
 {
-	typename T::const_iterator it;
-	typename T::const_iterator end = container.end();
-	
-	for (it = container.begin(); it != end; ++it)
-	{
-		if (*it == to_find)
-			return it;
-	}
-	return --it;
+	return std::find(container.begin(), container.end(), to_find);
 }
 
 #endif
--- ex00/main.cpp
+++ ex00/main.cpp
@@ -6,27 +6,29 @@
 /*   By: vfurmane <vfurmane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/01/16 12:05:46 by vfurmane          #+#    #+#             */
-/*   Updated: 2022/01/16 14:38:16 by vfurmane         ###   ########.fr       */
+/*   Updated: 2022/02/04 14:30:06 by vfurmane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <list>
+#include <vector>
 #include "easyfind.hpp"
 
 int	main(void)
 {
 	int				foo[] = {42, 21, 1337, 0, 100};
-	std::list<int>	bar(foo, foo + sizeof foo / sizeof *foo);
-	std::cout << *easyfind(bar, 42) << std::endl;
-	std::cout << *easyfind(bar, 100) << std::endl;
-	/* std::find                                        */
-	/* https://cplusplus.com/reference/algorithm/find/  */
-	/*                                                  */
-	/* Return value                                     */
-	/* An iterator to the first element in the range    */
-	/* that compares equal to val.                      */
-	/* If no elements match, the function returns last. */
-	std::cout << *easyfind(bar, 44) << std::endl;
+	{
+		std::list<int>	bar(foo, foo + sizeof foo / sizeof *foo);
+		std::cout << *easyfind(bar, 42) << std::endl;
+		std::cout << *easyfind(bar, 100) << std::endl;
+		std::cout << *easyfind(bar, 44) << std::endl;
+	}
+	{
+		std::vector<int>	bar(foo, foo + sizeof foo / sizeof *foo);
+		std::cout << *easyfind(bar, 42) << std::endl;
+		std::cout << *easyfind(bar, 100) << std::endl;
+		std::cout << *easyfind(bar, 44) << std::endl;
+	}
 	return 0;
 }
